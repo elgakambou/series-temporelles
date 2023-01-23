e770e5eb9fe3674b76b6a1c49922e9b6b1ae646c
Mario Morawski <mario.morawski@grenoble-inp.org>
2023-01-24 00:31:29 +0100

    ajout des print et copy

--- cAM.hpp
+++ cAM.hpp
@@ -7,4 +7,16 @@ public:
     int q;
     cU * processU;
     cAM(int the_q, cU * theprocessU);
+    void mPrint() {
+            std::cout << "cAM  : \n";
+            std::cout << "parameter q : " << q << "\n";
+            std::cout << "parameter processU : " << "\n";
+            processU->mPrint();
+        }
+    cAM* copy() {
+        int qCopy = q;
+        cU * processUCopy = processU.copy();
+        cAM* copy = new cAM(qCopy, processUCopy);
+        return copy;
+    }
 };
--- cARCH.hpp
+++ cARCH.hpp
@@ -5,6 +5,18 @@ class cARCH : public cAbstractSigma
 {
 public:
     int p;
-    cY * processy;
+    cY * processY;
     cARCH(int the_p, cY* theprocessY);
+    void mPrint() {
+            std::cout << "cARCH  : \n";
+            std::cout << "parameter p : " << p << "\n";
+            std::cout << "parameter processY : " << "\n";
+            processY->mPrint();
+        }
+    cARCH* copy() {
+        int pCopy = p;
+        cY * processYCopy = processY.copy();
+        cARCH* copy = new cARCH(pCopy, processYCopy);
+        return copy;
+    }
 };
