2db7e9f002f21337d609fb88b863077a65fb9c45
Mario Morawski <mario.morawski@grenoble-inp.org>
2023-01-24 09:33:52 +0100

    modif methode print

--- cAM.hpp
+++ cAM.hpp
@@ -7,16 +7,6 @@ public:
     int q;
     cU * processU;
     cAM(int the_q, cU * theprocessU);
-    void mPrint() {
-            std::cout << "cAM  : \n";
-            std::cout << "parameter q : " << q << "\n";
-            std::cout << "parameter processU : " << "\n";
-            processU->mPrint();
-        }
-    cAM* copy() {
-        int qCopy = q;
-        cU * processUCopy = processU.copy();
-        cAM* copy = new cAM(qCopy, processUCopy);
-        return copy;
-    }
+    cAM::cAM(cAM* other);
+    void mPrint();
 };
--- cARCH.hpp
+++ cARCH.hpp
@@ -7,16 +7,6 @@ public:
     int p;
     cY * processY;
     cARCH(int the_p, cY* theprocessY);
-    void mPrint() {
-            std::cout << "cARCH  : \n";
-            std::cout << "parameter p : " << p << "\n";
-            std::cout << "parameter processY : " << "\n";
-            processY->mPrint();
-        }
-    cARCH* copy() {
-        int pCopy = p;
-        cY * processYCopy = processY.copy();
-        cARCH* copy = new cARCH(pCopy, processYCopy);
-        return copy;
-    }
+    cARCH(cARCH * other);
+    void mPrint();
 };
